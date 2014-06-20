30f14488920fadae455700d79954ccb099270eb1
Arlan Jaska <ajaska@berkeley.edu>
2014-06-20 14:52:18 -0700

    Remove seq checking

--- nessusapi/session.py
+++ nessusapi/session.py
@@ -27,15 +27,12 @@ class Session:
     def request(self, path, **kwargs):
         if hasattr(self, 'token'):
             kwargs['token'] = self.token 
-        kwargs['seq'] = random.randrange(1000000)
         url = 'https://{0}:{1}/{2}'.format(self.host,self.port,path)
         request = Request(url, urlencode(kwargs))
         try:
             response = urlopen(request)
             response_data = xmltodict.parse(response.read())['reply']
-            if int(response_data['seq']) != kwargs['seq']:
-                raise Exception("Unique number did not match!")
-            elif response_data['status'] != 'OK':
+            if response_data['status'] != 'OK':
                 raise AuthenticationError("Invalid credentials")
             return response_data['contents']
         except HTTPError as e:
