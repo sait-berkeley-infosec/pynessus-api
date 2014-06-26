49f41a63ded2677b68f99ad11dc6987518764d47
Arlan Jaska <ajaska@berkeley.edu>
2014-06-25 18:11:11 -0700

    Hide password input

--- nessusinterface/session.py
+++ nessusinterface/session.py
@@ -1,16 +1,22 @@
 from nessusapi.session import Session
+from getpass import getpass
 
 def authenticate():
     Session( prompt("Username"),
-             prompt("Password"),
+             prompt("Password", hidden=True),
              prompt("Host", "127.0.0.1"),
              prompt("Port", "8834") )
 
-def prompt(text, default=None):
+def prompt(text, default=None, hidden=False):
     choice = ""
     while choice == "":
-        choice = raw_input("{0}{1}: ".format(text, " (default: {0})".format(default) if default else ""))
-        if choice == "" and not default is None:
+        prompt = "{0}{1}: ".format(text, " (default: {0})".format(default) if default else "")
+        if hidden:
+            choice = getpass(prompt)
+        else:
+            choice = raw_input(prompt)
+
+        if choice == "" and default is not None:
             return default
     return choice
             
