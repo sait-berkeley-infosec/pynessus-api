2c4bb848fba87639cd5a84cd7278c7a15c9c7e4f
Arlan Jaska <ajaska@berkeley.edu>
2014-06-26 18:43:43 -0700

    Improve report selection UI

--- nessusinterface/report.py
+++ nessusinterface/report.py
@@ -6,11 +6,12 @@ def select_report():
     reports = [report for report in report_data]
     reports.reverse()
     selected = None
-    print("Please select a report (most recent first)")
+    print("\nPlease select a report (most recent first)")
     index = 0
     while selected is None:
-        print("\n"+"="*75)
-        print("Showing reports {0} to {1} (of {2})".format(index+1,index+10,len(reports)))
+        print("="*75)
+        print("Showing reports {0} to {1} (of {2}) Most recent reports first."
+               .format(index+1,min(index+10,len(reports)),len(reports)))
         for i in range(10):
             try:
                 timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(reports[index+i]['timestamp'])))
@@ -21,6 +22,7 @@ def select_report():
             except IndexError:
                 pass
         choice = raw_input("Selection (n for next page, p for prev): ")
+        print("\n")
         if choice == 'n' and len(reports) > index+10:
             index += 10
         elif choice == 'p' and index >= 10:
