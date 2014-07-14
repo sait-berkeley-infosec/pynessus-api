da7470930b9685fffefbd91470d2d0e4a0e079ed
Nicholas Wold <nickw254@gmail.com>
2014-07-13 22:48:14 -0700

    [UNTESTED] Support for environment variables

--- nessusinterface/session.py
+++ nessusinterface/session.py
@@ -1,13 +1,14 @@
 from getpass import getpass
+import os
 
 from nessusapi.session import Session
 
 def authenticate():
     print("Connecting to Nessus API")
-    host=_prompt("Host", "127.0.0.1")
-    port=_prompt("Port", "8834")
-    Session(_prompt("Username"),
-            _prompt("Password", hidden=True),
+    host=os.getenv('NESSUS_HOST') or _prompt("Host", "127.0.0.1")
+    port=os.getenv('NESSUS_PORT') or _prompt("Port", "8834")
+    Session(os.getenv('NESSUS_USER') or _prompt("Username"),
+            os.getenv('NESSUS_PASS') or _prompt("Password", hidden=True),
             host,
             port)
 
