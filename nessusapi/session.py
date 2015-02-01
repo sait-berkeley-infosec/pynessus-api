b5496e25f9436c1da25cbbf6ac2c5daea9b85e03
Arlan Jaska <ajaska@berkeley.edu>
2015-02-01 13:40:50 -0800

    Fix and add new tests for test_session

--- nessusapi/session.py
+++ nessusapi/session.py
@@ -12,7 +12,7 @@ class Session(object):
     def close(self):
         """Log out of the API and invalidate the token"""
         if self.token is None:
-            return True
+            raise AuthenticationError('Invalid session')
         if self.request('logout') == 'OK':
             self.token = None
             return True
@@ -21,12 +21,15 @@ class Session(object):
     def request(self, path, **kwargs):
         """Make a request to a path with specified kwargs"""
         if hasattr(self, 'token'):
-            kwargs['token'] = self.token 
+            kwargs['token'] = self.token
+        elif path != 'login':
+            raise AuthenticationError('Invalid session')
 
         url = 'https://{0}:{1}/{2}'.format(self.host,self.port,path)
         r = requests.post(url, verify=self.verifySSL, data=kwargs)
+
         if r.status_code != requests.codes.ok:
-            raise r.raise_for_status()
+            r.raise_for_status()
 
         response_data = xmltodict.parse(r.text)['reply']
         if response_data['status'] != 'OK':
--- tests/test_session.py
+++ tests/test_session.py
@@ -5,70 +5,74 @@ try:
     import unittest.mock as mock
 except ImportError:
     import mock
+
 import unittest
-try: 
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
+import nessusapi.session
 
-from nessusapi.session import Session
-from nessusapi.session import Request
+xml_login_success = ('<?xml version="1.0"?> <reply>'
+                     "<status>OK</status>"
+                     "<contents><token>abcdef01</token>"
+                     "<user>"
+                     "<name>admin</name>"
+                     "<admin>TRUE</admin>"
+                     "</user></contents>"
+                     "</reply>")
 
-class SessionTestCase(unittest.TestCase):
-    @mock.patch('nessusapi.session.random')
-    @mock.patch('nessusapi.session.urlopen')
-    @mock.patch('nessusapi.session.Request')
-    def test_request(self, mock_request, mock_urlopen, mock_random):
-        mock_random.randrange.return_value = 2813
-        mock_urlopen.return_value = StringIO('<?xml version="1.0"?> <reply>'
-                                             "<seq>2813</seq>"
-                                             "<status>OK</status>"
-                                             "<contents><token>abcdef01</token>"
-                                             "<user>"
-                                             "<name>admin</name>"
-                                             "<admin>TRUE</admin>"
-                                             "</user></contents>"
-                                             "</reply>")
-        session = Session('user', 'pass', '192.0.2.7', '8981')
-        mock_urlopen.return_value = StringIO('<?xml version="1.0"?> <reply>'
-                                             "<seq>2813</seq>"
-                                             "<status>OK</status>"
-                                             "<contents>OK</contents>"
-                                             "</reply>")
-        session.request('test/url', arg1="arg1value", arg2="arg2value")
-        mock_request.assert_called_with('https://192.0.2.7:8981/test/url',
-                                        'arg1=arg1value&arg2=arg2value'
-                                        '&token=abcdef01&seq=2813')
+xml_login_error   = ('<?xml version="1.0"?> <reply>'
+                     "<status>ERROR</status>"
+                     "<contents>Invalid login</contents>"
+                     "</reply>")
 
-    @mock.patch('nessusapi.session.random')
-    @mock.patch('nessusapi.session.urlopen')
-    @mock.patch('nessusapi.session.Request')
-    def test_auth(self, mock_request, mock_urlopen, mock_random):
-        mock_random.randrange.return_value = 2811
-        mock_urlopen.return_value = StringIO('<?xml version="1.0"?> <reply>'
-                                             "<seq>2811</seq>"
-                                             "<status>OK</status>"
-                                             "<contents><token>ce65ea7</token>"
-                                             "<user>"
-                                             "<name>admin</name>"
-                                             "<admin>TRUE</admin>"
-                                             "</user></contents>"
-                                             "</reply>")
-        session = Session('user', 'pass', '192.0.2.3', '8980')
-        mock_request.assert_called_once_with('https://192.0.2.3:8980/login',
-                                        'login=user&password=pass&seq=2811')
-        self.assertEqual(session.token, "ce65ea7")
+class TestSession(unittest.TestCase):
+    @mock.patch('nessusapi.session.Session.request')
+    def test_init(self, mock_request):
+        mock_request.return_value = {'token': 'test_token'}
 
-        mock_random.randrange.return_value = 2817
-        mock_urlopen.return_value = StringIO('<?xml version="1.0"?> <reply>'
-                                             "<seq>2817</seq>"
-                                             "<status>OK</status>"
-                                             "<contents>OK</contents>"
-                                             "</reply>")
-        self.assertTrue(session.close())
-        mock_request.assert_called_with('https://192.0.2.3:8980/logout',
-                                        'token=ce65ea7&seq=2817')
-        self.assertIsNone(session.token)
+        s = nessusapi.session.Session("user", "pwd", "host", 8834)
 
-if __name__ == '__main__':
-    unittest.main()
+        mock_request.assert_called_once_with("login",**{"login":"user",
+                                                        "password": "pwd"})
+
+        self.assertEqual(s.token, 'test_token')
+        self.assertEqual(s.host, 'host')
+        self.assertEqual(s.port, 8834)
+
+    @mock.patch('nessusapi.session.Session.request')
+    def test_close(self, mock_request):
+        from nessusapi.session import AuthenticationError 
+        mock_request.return_value = {'token': 'test_token'}
+        s = nessusapi.session.Session("user", "pwd", "host", 8834)
+        self.assertIsNotNone(s.token)
+        
+        mock_request.return_value = "OK"
+        self.assertTrue(s.close())
+        self.assertIsNone(s.token)
+        mock_request.assert_called_with("logout")
+
+        self.assertRaises(AuthenticationError, s.close)
+
+    @mock.patch('nessusapi.session.requests.post')
+    def test_request_failure(self, mock_post):
+        from requests.exceptions import ConnectionError
+        def _raise(ex):
+            raise ex
+
+        mock_post.return_value=mock.Mock(status_code=403,
+                                         raise_for_status=lambda: _raise(ConnectionError))
+        self.assertRaises(ConnectionError, nessusapi.session.Session,
+                          *("user", "pwd", "host", 8834))
+
+        mock_post.return_value=mock.Mock(status_code=501,
+                                         raise_for_status=lambda: _raise(ConnectionError))
+        self.assertRaises(ConnectionError, nessusapi.session.Session,
+                          *("user", "pwd", "host", 8834))
+
+    @mock.patch('nessusapi.session.requests.post')
+    def test_request_bad_login(self, mock_post):
+        from nessusapi.session import AuthenticationError 
+        mock_post.return_value=mock.Mock(status_code=200,
+                                         raise_for_status=lambda: None,
+                                         text=xml_login_error)
+        self.assertRaises(AuthenticationError, nessusapi.session.Session,
+                          *("user", "pwd", "host", 8834))
+
