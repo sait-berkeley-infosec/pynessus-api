089b2c4f464b89199a20d537ac8b33deb578f04e
Arlan Jaska <ajaska@berkeley.edu>
2015-06-04 18:29:22 -0700

    Improve testing

--- tests/test_nessus.py
+++ tests/test_nessus.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-try:
-    import unittest.mock as mock
-except ImportError:
-    import mock
+import mock
 
-import unittest
 import nessusapi.nessus
 
-class TestNessus(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.real_session = nessusapi.nessus.Session
-        nessusapi.nessus.Session = mock.Mock()
 
-    @classmethod
-    def tearDownClass(cls):
-        nessusapi.nessus.Session = cls.real_session
-
-    def test_init(self):
-        mock_session = nessusapi.nessus.Session
+class TestNessus():
+    @mock.patch('nessusapi.nessus.Session')
+    def test_init(self, mock_session):
         nessusapi.nessus.Nessus('user', 'pw')
         mock_session.assert_called_with('user', 'pw', host='localhost',
                                         port=8834, verifySSL=True)
@@ -33,14 +21,21 @@ class TestNessus(unittest.TestCase):
         mock_session.assert_called_with('user', 'pw', host='host',
                                         port=30, verifySSL=False)
 
+    @mock.patch('nessusapi.nessus.Session')
+    def test_logout(self, mock_session):
+        nessus = nessusapi.nessus.Nessus('user', 'pw')
+        nessus.logout()
+
+        assert mock.call('user', 'pw', host='localhost', verifySSL=True, port=8834) in mock_session.mock_calls
 
     @mock.patch('nessusapi.nessus.Nessus.request_list')
-    def test_reports(self, mock_request_list):
+    @mock.patch('nessusapi.nessus.Session')
+    def test_reports(self, mock_session, mock_request_list):
         nessus = nessusapi.nessus.Nessus('user', 'pw')
 
         mock_request_list.return_value = []
-        reports = nessus.reports
-        self.assertEqual(reports, [])
+        assert nessus.reports == []
+
         mock_request_list.assert_called_with('report/list', 'reports',
                                              'report')
 
@@ -48,11 +43,11 @@ class TestNessus(unittest.TestCase):
                                            'readableName': 'report1',
                                            'status': 'completed'}]
         reports = nessus.reports
-        self.assertEqual(len(reports), 1)
-        self.assertEqual(reports[0].uuid, '123-ab')
-        self.assertEqual(reports[0].timestamp, 5)
-        self.assertEqual(reports[0].name, 'report1')
-        self.assertEqual(reports[0].status, 'completed')
+        assert len(reports) == 1
+        assert reports[0].uuid == '123-ab'
+        assert reports[0].timestamp == 5
+        assert reports[0].name == 'report1'
+        assert reports[0].status == 'completed'
 
         mock_request_list.return_value = [{'name': '321-ba', 'timestamp': '5',
                                            'readableName': 'report2',
@@ -62,6 +57,4 @@ class TestNessus(unittest.TestCase):
                                            'status': 'stopped'}]
 
         reports = nessus.reports
-        self.assertEqual(len(reports), 2)
-
-
+        assert len(reports) == 2
